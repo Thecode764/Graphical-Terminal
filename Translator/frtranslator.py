1607f405d4cad3fe3767185a92df32a6e7a58381
Im Code <141347375+Thecode764@users.noreply.github.com>
2023-09-10 18:21:50 +0330

    Create frtranslator.py

---
+++ Translator/frtranslator.py
@@ -0,0 +1,10 @@
+from googletrans import Translator
+
+def translate_text(text):
+    translator = Translator(service_urls=['translate.google.com'])
+    translation = translator.translate(text, dest='fr')
+    return translation.text
+
+text = input("Enter the text to translate: ")
+translated_text = translate_text(text)
+print("Translated text: ", translated_text)
