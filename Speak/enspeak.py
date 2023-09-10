9926d030603f979ecedce1e0a3fb285f362ebae1
Im Code <141347375+Thecode764@users.noreply.github.com>
2023-09-10 18:39:24 +0330

    Create enspeak.py

---
+++ Speak/enspeak.py
@@ -0,0 +1,15 @@
+from googletrans import Translator
+from gtts import gTTS
+import os
+
+def translate_and_speak(text):
+    translator = Translator(service_urls=['translate.google.com'])
+    translation = translator.translate(text, dest='en')
+    translated_text = translation.text
+
+    tts = gTTS(text=translated_text, lang='en')
+    tts.save("translation.mp3")
+    os.system("start translation.mp3")
+
+text = input("Enter the text to translate and speak: ")
+translate_and_speak(text)
