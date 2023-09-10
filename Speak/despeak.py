0d9b7b305a62b3e4fde3b607765d5d074142d11c
Im Code <141347375+Thecode764@users.noreply.github.com>
2023-09-10 18:42:25 +0330

    Create despeak.py

---
+++ Speak/despeak.py
@@ -0,0 +1,15 @@
+from googletrans import Translator
+from gtts import gTTS
+import os
+
+def translate_and_speak(text):
+    translator = Translator(service_urls=['translate.google.com'])
+    translation = translator.translate(text, dest='de')
+    translated_text = translation.text
+
+    tts = gTTS(text=translated_text, lang='de')
+    tts.save("translation.mp3")
+    os.system("start translation.mp3")
+
+text = input("Enter the text to translate and speak: ")
+translate_and_speak(text)
